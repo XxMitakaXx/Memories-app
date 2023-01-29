6192ac801f76d5340756de40611bd056350a1b0d
XxMitakaXx <mitakavarbanov05@gmail.com>
2023-01-29 16:42:36 +0200

    Push

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -28,12 +28,18 @@ export const createPost = async (req, res) => {
 }
 
 export const updatePost = async (req, res) => {
-    const { id: _id } = res.params;
-	const post = req.body;
+	const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
 
-	if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    const updatePost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
-	res.json(updatePost);
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
+}
+
+export const deletePost = () => {
+
 }
