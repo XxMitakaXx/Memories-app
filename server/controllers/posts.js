7c185b28ea51f078072c2ca8462fad1cd6b23aae
XxMitakaXx <mitakavarbanov05@gmail.com>
2023-01-29 20:19:46 +0200

    Push

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -45,7 +45,7 @@ export const deletePost = async (req, res) => {
 
 	if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-	await PostMessage.findByIdAndDelete(id);
+	await PostMessage.findByIdAndRemove(id);
 
 	res.json({ message: 'Post deleted successfully' })
 }
