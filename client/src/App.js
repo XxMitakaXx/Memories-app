1bc3716a667092f5675047c1b07acc2e90964a7f
XxMitakaXx <mitakavarbanov05@gmail.com>
2023-01-30 00:47:52 +0200

    Push

--- client/src/App.js
+++ client/src/App.js
@@ -19,10 +19,7 @@ const App = () => {
 
 	return (
 		<Container maxidth="lg">
-			<AppBar className={classes.appBar} position="static" color="inherit">
-				<Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-			    <img className={classes.image} src={memories} alt="memories" height="60" />
-			</AppBar>
+			
 		    <Grow in>
 				<Container>
 					<Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { AppBar, Typography } from '@material-ui/core';
+
+const Navbar = ()  => {
+    const classes = useStyle();
+
+	return (
+	    <AppBar className={classes.appBar} position="static" color="inherit">
+	    	<Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+	        <img className={classes.image} src={memories} alt="memories" height="60" />
+	    </AppBar>
+	)
+}
+
+export default Navbar;
