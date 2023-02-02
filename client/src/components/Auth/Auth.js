654b8bc95c392e5c72c5daa16ea58eb4d9287203
XxMitakaXx <mitakavarbanov05@gmail.com>
2023-02-02 20:30:01 +0200

    Push

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,12 +1,47 @@
 import React from "react";
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from "@material-ui/core";
+
+import useStyles from './styles'
+import LockedOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 const Auth = () => {
-    const state = null;
+    const classes = useStyles();
+
+	const isSignUp = false;
 	
+    const HandleSubmit = () => {
+
+	}
+
+	const HandleChange = () => {
+
+	}
+
 	return (
-		<div>
-			AUTH
-		</div>
+		<Container component='main' maxWidth='xs'>
+            <Paper className={classes.paper} elevation={3}>
+				<Avatar className={classes.avatar}>
+					<LockedOutlinedIcon />
+				</Avatar>
+				<Typography variant="h5">{isSignUp ? 'Sign Un' : 'Sign In'}</Typography>
+				<form className={classes.form} onSubmit={HandleSubmit}>
+                    <Grid container spacing={2}>
+						{
+							isSignUp && (
+								<>
+								    <Grid>
+								        <TextField name='firstName' label='First Name' handleChange={HandleChange} autoFocus xs={6} />
+									</Grid>
+									<Grid>
+								        <TextField name='firstName' label='First Name' handleChange={HandleChange} autoFocus xs={6} />
+									</Grid>
+								</>
+							)
+						}
+					</Grid>
+				</form>
+			</Paper>
+		</Container>
 	)
 }
 
